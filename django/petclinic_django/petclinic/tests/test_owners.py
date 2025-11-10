f06ed357a41f2b58083d5f10ccf423750143e174
itsmealessandro <129402291+itsmealessandro@users.noreply.github.com>
2025-11-10 12:01:14 +0100

    update owner test + better print for owner

--- django/petclinic_django/petclinic/tests/test_owners.py
+++ django/petclinic_django/petclinic/tests/test_owners.py
@@ -4,10 +4,48 @@ from petclinic.models import Owner
 class OwnerTests(BaseTestCase):
 
     def test_create_owner(self):
+
+        print("######################################")
+        print("OWNER TESTING")
+
+        print("-------------- create owner --------------")
         owner = self.create_owner()
         self.assertEqual(owner.first_name, "Mario")
 
     def test_update_owner(self):
-        owner = self.create_owner("MARIO_UPDATE")
+
+        print("-------------- update owner --------------")
+        print("update owner")
+        FIRST_NAME = "MARIO_UPDATED"
+        LAST_NAME = "ROSSI_UPDATED"
+        ADDRESS = "ADDRESS_UPDATED"
+        CITY = "CITY_UPDATED"
+        TELEPHONE = "TELEPHONE_UPDATED"
+        owner = self.create_owner()
+        print("old owner:")
+        print(owner)
         querySet = Owner.objects.filter(id=owner.id)
-        querySet.update(first_name="MARIO_UPDATED")
+        querySet.update(
+            first_name=FIRST_NAME,
+            last_name=LAST_NAME,
+            address=ADDRESS,
+            city=CITY,
+            telephone=TELEPHONE # no check for number sequence 
+        )
+
+        # print("owner before refresh_from_db")
+        # print(owner)
+
+        # Refresh instance from DB to get updated values
+        owner.refresh_from_db()
+
+        print("owner after refresh_from_db")
+        print(owner)
+        self.assertEqual(owner.first_name, FIRST_NAME)
+        self.assertEqual(owner.last_name, LAST_NAME)
+        self.assertEqual(owner.address, ADDRESS)
+        self.assertEqual(owner.city, CITY)
+        self.assertEqual(owner.telephone, TELEPHONE)
+
+
+
