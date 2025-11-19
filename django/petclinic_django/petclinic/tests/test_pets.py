f7dd21c295ce7eca9b5c0d782fe2e8ae567c733d
itsmealessandro <129402291+itsmealessandro@users.noreply.github.com>
2025-11-19 22:47:19 +0100

    pet test and better owner test delete

---
+++ django/petclinic_django/petclinic/tests/test_pets.py
@@ -0,0 +1,53 @@
+# write the pet test like for the owner test
+
+from petclinic.tests.base_test import BaseTestCase
+from petclinic.models import Pet
+
+""" CRUD testing for pet """
+""" test functionts runs in an lexicographic order"""
+
+class PetTests(BaseTestCase):
+    def test_create_pet(self):
+
+        print("######################################")
+        print("PET TESTING")
+
+        print("-------------- create pet --------------")
+        pet = self.create_pet()
+        self.assertEqual(pet.id, 1) # id starts from 1 in djangoDB
+
+
+    def test_read_pet(self):
+        print("-------------- read pet --------------")
+
+
+    def test_update_pet(self):
+        print("-------------- update pet --------------")
+        NAME = "FIDO_UPDATED"
+        BIRTH_DATE = "2020-01-01"
+        pet = self.create_pet()
+        print("old pet:")
+        print(pet)
+        querySet = Pet.objects.filter(id=pet.id)
+        querySet.update(
+            name=NAME,
+            birth_date=BIRTH_DATE
+        )
+
+        # Refresh instance from DB to get updated values
+        pet.refresh_from_db()
+
+        print("pet after refresh_from_db")
+        print(pet)
+        self.assertEqual(pet.name, NAME)
+        self.assertEqual(str(pet.birth_date), BIRTH_DATE)
+
+
+    def test_delete_pet(self):
+        print("-------------- delete pet --------------")
+        pet= self.create_pet();
+        querySet = Pet.objects.filter(id=pet.id)
+        querySet.delete()
+        with self.assertRaises(Pet.DoesNotExist):
+            Pet.objects.get(id=pet.id)
+
